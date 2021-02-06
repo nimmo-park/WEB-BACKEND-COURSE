69cd2bb924f2d217647c7e0df89399ccc0010bd4
nimmo-park <ggomssngma@naver.com>
2021-02-06 23:41:37 +0900

    []배열

--- first/src/first/Hello.java
+++ first/src/first/Hello.java
@@ -1,10 +1,18 @@
 package first;
 
+import java.util.List;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TEST");
+		
+		String[] list = {"1","2"};
+		
+		for(int i = 0 ; i < list.length; i++) {
+			System.out.println(list[i]);
+		}
 	}
 
 }
