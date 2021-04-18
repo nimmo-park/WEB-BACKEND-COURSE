3f76f23a7cde0cb7e1304b7cdc5eb04f0af23351
nimmo-park <ggomssngma@naver.com>
2021-04-18 12:33:06 +0900

    back-joon

---
+++ back-joon-pro/src/com/backjoon/problem_two_sum.java
@@ -0,0 +1,16 @@
+package com.backjoon;
+
+import java.util.Scanner;
+
+public class problem_two_sum {
+
+	public static void main(String[] args) {
+		int a,b;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a-b);
+	}
+}
